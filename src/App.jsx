57455312a694b0f0278088eddce82d8462c7cc26
Rohit Kumar Morya <moryarohit7976@gmail.com>
2025-06-17 12:01:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  
+
+  return (
+    
+      <div className='text-5xl font-bold text-green-500'>
+        namaste react lets build netflix gpt
+       </div>
+
+  )
+}
+
+export default App
