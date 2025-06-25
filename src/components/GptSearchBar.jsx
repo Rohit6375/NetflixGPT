19de7a12850b43addee068927cf7f2dc4e448b42
Rohit Kumar Morya <moryarohit7976@gmail.com>
2025-06-25 17:48:19 +0530

    fixed gptsearch bar

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -40,7 +40,7 @@ const GptSearchBar = () => {
   <div>
     <div className="pt-[40%] md:p-[10%] flex justify-center">
       <form className="md:w-1/2 w-full bg-black/80 grid grid-cols-12 rounded-xl" onSubmit={(e) => e.preventDefault()}>
-        <input ref={searchText} type="text" className="p-4 m-4 bg-white/95 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder} />
+        <input ref={searchText} type="text" className="md:p-4 md:m-4 m-2 p-2 bg-white/95 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder} />
         <button
           className={`py-2 px-4 font-bold hover:cursor-pointer text-white rounded-lg col-span-3 m-4 ${
             loading ? "bg-gray-500 cursor-not-allowed" : "bg-red-600/80 hover:bg-red-600/50"
